364af2fb17a8add53a9f893ae1e0bc36bc3181d6
Olivia Borgstrom <olivia.borgstrom@student.manchester.ac.uk>
2022-11-30 14:02:50 +0000

    Add tests for retrieving the cart from the FarmerNPC
    Created a test class for testing if the farmer will place a hand cart
    when the player requests him to. The tests assert that the player must
    be at least level 300 in order to retrieve the cart from Karl. This is
    to enforce our requirement to allow only a subset of players to test
    the feature.
    
    These tests should fail at this commit.

---
+++ tests/games/stendhal/server/maps/ados/forest/FarmerNPCTest.java
@@ -0,0 +1,95 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2011 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.ados.forest;
+
+import static org.junit.Assert.assertEquals;
+import static utilities.SpeakerNPCTestHelper.getReply;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.fsm.Engine;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.MockStendlRPWorld;
+import utilities.PlayerTestHelper;
+import utilities.QuestHelper;
+import utilities.ZonePlayerAndNPCTestImpl;
+import utilities.RPClass.BlockTestHelper;
+/**
+ * Test getting hand carts.
+ * @author Olivia Borgstrom
+ */
+public class FarmerNPCTest extends ZonePlayerAndNPCTestImpl{
+
+	private Player player = null;
+	private SpeakerNPC npc = null;
+	private Engine en = null;
+	private static final String ZONE_NAME = "0_ados_forest_w2";
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		QuestHelper.setUpBeforeClass();
+		BlockTestHelper.generateRPClasses();
+		PlayerTestHelper.generatePlayerRPClasses();
+
+		setupZone(ZONE_NAME);
+	}
+
+	@Before
+	public void setUp() {
+		final StendhalRPZone zone = new StendhalRPZone("0_ados_forest_w2",200, 200);
+		MockStendlRPWorld.get().addRPZone(zone);
+		new FarmerNPC().configureZone(zone, null);
+
+		player = PlayerTestHelper.createPlayer("bob");
+	}
+	
+	public FarmerNPCTest() {
+		setNpcNames("Karl");
+		addZoneConfigurator(new FarmerNPC(), ZONE_NAME);
+	}
+	
+	/**
+	 * Tests for getting cart when you are low level.
+	 */
+	@Test
+	public void testGetCartLowLevel() {
+		final SpeakerNPC npc = getNPC("Karl");
+		final Engine en = npc.getEngine();
+		player.setLevel(1);
+		en.step(player, "hi");
+		en.step(player, "cart");
+		assertEquals("Sorry this item is for high level players only!", getReply(npc));
+		en.step(player, "bye");
+	}
+
+	
+	/**
+	 * Tests for getting cart when you are high level.
+	 */
+	@Test
+	public void testGetCartHighLevel() {
+		final SpeakerNPC npc = getNPC("Karl");
+		final Engine en = npc.getEngine();
+		
+		player.setLevel(300);
+		en.step(player, "hi");
+		en.step(player, "cart");
+		assertEquals("Here is a cart!", getReply(npc));
+		en.step(player, "bye");
+	}
+
+}
+
