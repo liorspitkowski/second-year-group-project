3e0820d56e30fefd8b35669d9a386abe83469ef8
Briana Neagu <briana.neagu@student.manchester.ac.uk>
2022-12-01 16:05:41 +0000

    Add more tests for the BankStatement class
    Added two more tests that check the functionality, including adding more
    than one item to a bank and adding same item to different banks.

--- tests/games/stendhal/server/entity/slot/BankStatementTest.java
+++ tests/games/stendhal/server/entity/slot/BankStatementTest.java
@@ -65,5 +65,81 @@ public class BankStatementTest {
 		// checks if it contains cheese, should return false
 		assertFalse(bs.getItemsFromBank(Banks.SEMOS).contains("cheese"));
 	}
+	
+	
+	/**
+	 * Tests for getItemsFromBank(), adds the same item to two different banks and then removes one of them and checks if it is present in bank statement
+	 */
+	@Test
+	public final void testAddTheSameItemInDifferentBanksAndRemoveOneFromBankStatement() {
+		// creates test world and player
+		StendhalRPZone localzone = new StendhalRPZone("testzone", 20, 20);
+		SingletonRepository.getRPWorld().addRPZone(localzone);
+		final Player player = PlayerTestHelper.createPlayer("briana");
+		StackableItem item = (StackableItem) SingletonRepository.getEntityManager().getItem("cheese");
+		
+		/// gives them an item "cheese" and places it in their SEMOS chest
+		player.equip(Banks.SEMOS.getSlotName(), item);
+		localzone.add(player);
+		assertFalse(player.isEquipped("cheese"));
+		assertEquals(1, player.getTotalNumberOf("cheese"));
+		
+		/// gives them an item "cheese" and places it in their ADOS chest
+		player.equip(Banks.ADOS.getSlotName(), item);
+		localzone.add(player);
+		assertFalse(player.isEquipped("cheese"));
+		assertEquals(2, player.getTotalNumberOf("cheese"));
+		
+		
+		// creates bank statement and checks if it contains cheese, should return true
+		BankStatement bs = new BankStatement(player);
+		assertTrue(bs.getItemsFromBank(Banks.SEMOS).contains("cheese"));
+
+		// clears chest ados (removing cheese)
+		player.getSlot("bank_ados").clear();
+		
+		
+		// checks if it contains cheese, should return true
+		assertTrue(bs.getItemsFromBank(Banks.SEMOS).contains("cheese"));
+	}
+	
+	/**
+	 * Tests for getItemsFromBank(), adds multiple items to banks chest and checks if it is present in bank statement
+	 */
+	@Test
+	public final void testAddMultipleItemsToBankStatement() {		
+		// creates test world and player
+		StendhalRPZone localzone = new StendhalRPZone("testzone", 20, 20);
+		SingletonRepository.getRPWorld().addRPZone(localzone);
+		final Player player = PlayerTestHelper.createPlayer("lior");
+		
+		/// gives them an item "cheese" and places it in their bank chest
+		StackableItem itemA = (StackableItem) SingletonRepository.getEntityManager().getItem("cheese");
+		player.equip(Banks.SEMOS.getSlotName(), itemA);
+		localzone.add(player);
+		assertFalse(player.isEquipped("cheese"));
+		assertEquals(1, player.getTotalNumberOf("cheese"));
+		
+		/// gives them an item "club" and places it in their bank chest
+		StackableItem itemB = (StackableItem) SingletonRepository.getEntityManager().getItem("coal");
+		player.equip(Banks.SEMOS.getSlotName(), itemB);
+		localzone.add(player);
+		assertFalse(player.isEquipped("coal"));
+		assertEquals(1, player.getTotalNumberOf("coal"));
+		
+		// creates bank statement and checks if it contains cheese, should be true
+		BankStatement bs = new BankStatement(player);
+		assertTrue(bs.getItemsFromBank(Banks.SEMOS).contains("cheese"));
+		assertTrue(bs.getItemsFromBank(Banks.SEMOS).contains("coal"));
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
