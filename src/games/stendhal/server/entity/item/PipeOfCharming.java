6e26187791781aa62076034430a1c4a8204f42c0
Boran Wang <boran.wang@student.manchester.ac.uk>
2022-12-01 17:43:38 +0000

    Add functions to PipeOfCharming class
    Production code to make the pipe
    usable and get description

--- src/games/stendhal/server/entity/item/PipeOfCharming.java
+++ src/games/stendhal/server/entity/item/PipeOfCharming.java
@@ -1,5 +1,6 @@
-/***************************************************************************
- *                   (C) Copyright 2003-2022 - Stendhal                    *
+/*
+ ***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -11,18 +12,69 @@
  ***************************************************************************/
 package games.stendhal.server.entity.item;
 
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.List;
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.List;
 import java.util.Map;
 
+//import games.stendhal.server.core.engine.SingletonRepository;
+//import games.stendhal.server.core.rule.EntityManager;
+
+//import org.apache.log4j.Logger;
+
+//import games.stendhal.common.Rand;
+//import games.stendhal.server.core.engine.SingletonRepository;
+//import games.stendhal.server.core.engine.StendhalRPZone;
+//import games.stendhal.server.core.rp.StendhalRPAction;
+//import games.stendhal.server.core.rule.EntityManager;
+import games.stendhal.server.entity.RPEntity;
+//import games.stendhal.server.entity.creature.AttackableCreature;
+//import games.stendhal.server.entity.creature.Creature;
+//import marauroa.common.game.RPObject;
+//import games.stendhal.server.entity.creature.Creature;
+
+/**
+ * Represents a creature summon staff.
+ */
 public class PipeOfCharming extends Item {
-	public PipeOfCharming(final String name, final String clazz, final String subclass, final Map<String, String> attributes) {
+
+
+
+	/**
+	 * Creates a new necromancer staff.
+	 *
+	 * @param name
+	 * @param clazz
+	 * @param subclass
+	 * @param attributes
+	 */
+	public PipeOfCharming(final String name, final String clazz, final String subclass,
+			final Map<String, String> attributes) {
 		super(name, clazz, subclass, attributes);
 	}
 
-
+	/**
+	 * Copy constructor.
+	 *
+	 * @param item
+	 *            item to copy
+	 */
 	public PipeOfCharming(final PipeOfCharming item) {
 		super(item);
 	}
+	public String describe() {
+		String text = "You see a pipe, it looks rather charming";
+		return text;
+	}
 
 
-	
+	@Override
+	public boolean onUsed(final RPEntity user) {
+		user.sendPrivateText("**MUSIC*PLACEHOLDER**");
+		return true;
+
+	}
 }
